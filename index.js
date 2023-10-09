71f32196f14e0c20d924914929501de42b46a31f
evansthitai <evansnt9619@gmail.com>
2023-10-09 11:13:13 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = drivers [0,1]
+
+const returnLastTwoDrivers = drivers[2,3]
+
+const selectDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+const fare = 59
+
+const fareMultiplier = function createFareMultiplier() {
+    let multiplier = 5
+    return ( fare* multiplier)
+}
+     console.log(fareMultiplier() + selectDrivers)
+
+     return selectDrivers
+
+
+  
+
+
+
+
+
+fareMultiplier() ;
+
+const fareDoubler = (fare *2)
+
+
+const fareTripler = (fare *3)
+
+const selectDifferentDrivers = (returnFirstTwoDrivers,returnLastTwoDrivers) 
+
+    return (drivers,returnFirstTwoDrivers)
+
+
+
